4212a742f2351f6b2cc7b99947734882e4b6a3ef
Mahmud Bello <75342173+mahmudsudo@users.noreply.github.com>
2026-08-18 07:42:48 +0100

    Add: string-encode-and-decode - submission-1

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-1.rs
@@ -0,0 +1,38 @@
+impl Solution {
+    pub fn encode(strs: Vec<String>) -> String {
+        let mut res = String::new();
+
+        for s in strs {
+            res.push_str(&s.len().to_string());
+            res.push('#');
+            res.push_str(&s);
+        }
+
+        res
+    }
+
+    pub fn decode(s: String) -> Vec<String> {
+        let bytes = s.as_bytes();
+        let mut res = Vec::new();
+        let mut i = 0;
+
+        while i < bytes.len() {
+            let mut j = i;
+
+            while bytes[j] != b'#' {
+                j += 1;
+            }
+
+            let len: usize = s[i..j].parse().unwrap();
+
+            let start = j + 1;
+            let end = start + len;
+
+            res.push(s[start..end].to_string());
+
+            i = end;
+        }
+
+        res
+    }
+}
